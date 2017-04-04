881edac6215d942ce538a03d5db98921bab7e5c8
miriampc <miiriampc@gmail.com>
2017-04-03 23:38:11 -0500

    validaciones mejoradas

--- js/main.js
+++ js/main.js
@@ -1,55 +1,81 @@
 function validateForm(){
-  var name=document.getElementById('name').value;
-  var lastname=document.getElementById('lastname').value;
-  var email=document.getElementById('input-email').value;
-  var password=document.getElementById('input-password').value;
-  var tipobici=document.getElementById('tipobici').value;
+  var name=document.getElementById('name');
+  var lastname=document.getElementById('lastname');
+  var email=document.getElementById('input-email');
+  var password=document.getElementById('input-password');
+  var tipobici=document.getElementById('tipobici');
   var mensaje="", id=0;
-
+  // var x=name.value.split("");
+  // var aa=[];
+  // aa=x.splice(0,1,"ds","dd")
+  // console.log(aa);
 //Funciones de Validación
   function validaNombreApellido(valor){
-    if(valor.length==0 || valor==null){
-      mensaje="Este campo es obligatorio";
+    valor.focus();
+    if(valor.value.trim()==""){
+      removeTooltip();
+      if (valor==name) {
+        mensaje="Debe ingresar su nombre";
+      }else {
+        mensaje="Debe ingresar su apellido";
+      }
+      valor.value="";
       creaTooltip(mensaje,id);
       return false;
-    }else if(!(/^([A-Za-zÑñá-úÁ-Ú ]+)$/.test(valor))){
+    }else if(!(/^([A-Za-zÑñá-úÁ-Ú ]+)$/.test(valor.value))){
+      removeTooltip();
       mensaje="Este campo solo acepta letras";
       creaTooltip(mensaje,id);
       return false;
-    }else if(!(/[^a-zá-ú-0-9\s]+([a-zá-ú ]{2,})/.test(valor))){
+    }else if(!(/[^a-zá-ú-0-9\s]+([a-zá-ú ]{2,})/.test(valor.value))){
+      removeTooltip();
       mensaje="La primera letra debe ser mayuscula";
       creaTooltip(mensaje,id);
       return false;
     }
     id++;
-    console.log(id);
+    removeTooltip();
     return true;
   }
 
   function validaEmail(valor){
-    if(valor.length==0 || valor==null){
+    valor.focus();
+    if(valor.value=="" || valor.value==null){
+      removeTooltip();
       mensaje="Debe ingresar su correo electrónico es obligatorio";
+      creaTooltip(mensaje,id);
+      return false;
+    }else if (!/([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})/gi.test(valor.value)) {
+      removeTooltip();
+      mensaje="Debe ingresar en formato correcto";
       creaTooltip(mensaje,id);
       return false;
     }
     id++;
     validaPassword(password);
   }
+
   function validaPassword(valor){
-    if(valor.length<6){
-      mensaje="Debe ingresar su password, debe tener almenos seis caracteres";
+    valor.focus();
+    if((valor.value).length<6){
+      removeTooltip();
+      mensaje="La contraseña debe tener almenos seis caracteres";
       creaTooltip(mensaje,id);
       return false;
-    }else if (valor=="098754" || valor=="123456" || valor=="password") {
-      mensaje="El password debe ser diferente de 123456, 098754 y password";
+    }else if (valor.value=="098754" || valor.value=="123456" || valor.value=="password") {
+      removeTooltip();
+      mensaje="La contraseña debe ser diferente de 123456, 098754 y password";
       creaTooltip(mensaje,id);
       return false;
     }
     id++;
     validaSeleccion(tipobici);
   }
+
   function validaSeleccion(valor){
-    if(valor==0){
+    valor.focus();
+    if(valor.value==0){
+      removeTooltip();
       mensaje="Debe seleccionar una opcion";
       creaTooltip(mensaje,id);
       return false;
@@ -60,16 +86,26 @@ function validateForm(){
     document.getElementById('input-email').value="";
     document.getElementById('input-password').value="";
     document.getElementById('tipobici').value=0;
+    name.focus();
   }
-  if(validaNombreApellido(name)){if(validaNombreApellido(lastname)){validaEmail(email)}return false;}
-
+  if(validaNombreApellido(name)){
+    if(validaNombreApellido(lastname)){
+      validaEmail(email)
+    } return false;
+  }
 }
 var divBox=document.getElementsByClassName('input-box');
 function creaTooltip(msj,id){
     var spanTool=document.createElement('span');
-    spanTool.id="tooltip"+id;
+    spanTool.setAttribute("class",'tool')
     var p=document.createElement('p');
     p.innerHTML=msj;
     spanTool.appendChild(p);
     divBox[id].appendChild(spanTool);
+}
+function removeTooltip(){
+   var tool=document.getElementsByClassName('tool');
+   for (var i = 0; i < tool.length; i++) {
+     tool[i].style.display="none";
+   }
 }
